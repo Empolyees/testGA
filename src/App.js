120f446c438190e39d6f1a600a232ba13961d60b
mkellner <martin.kellner62@gmail.com>
2025-03-03 18:29:57 +0100

    GA test

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
-import './App.css';
+import { useEffect } from "react";
+import ReactGA from "react-ga4";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <button>
-          Click me
-        </button>
-      </header>
-    </div>
-  );
+    useEffect(() => {
+        ReactGA.initialize("G-K3DDYP9KGG"); // Sem vlož svůj GA měřící kód
+        ReactGA.send("pageview"); // Odeslání události při načtení stránky
+    }, []);
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <button onClick={() => ReactGA.event({ category: "User", action: "Clicked button" })}>
+                    Click me
+                </button>
+            </header>
+        </div>
+    );
 }
 
 export default App;
